6ebcbc83ea86fe0bff09716c294a2ad4dcb31284
Suji Kim <117288953+kkkimsuji@users.noreply.github.com>
2026-03-24 21:38:37 +0900

    refactor: clean up del_form.php and fix HTML tag errors

--- del_form.php
+++ del_form.php
@@ -1,24 +1,39 @@
+<?php
+session_start();
+// 로그인 여부 확인이 필요하다면 여기에 추가할 수 있습니다.
+$idx = isset($_REQUEST['idx']) ? (int)$_REQUEST['idx'] : 0;
+
+if ($idx === 0) {
+    echo "<script>alert('잘못된 접근입니다.'); history.back();</script>";
+    exit;
+}
+?>
 <!DOCTYPE html>
-<html>
-<!--http://syaic12.cafe24.com/kimsuji/board/del_form.php  -->
-    <head>
-    <link rel = "stylesheet" href = "table.css">
-    </head>
-   
-    <body>
-        <center>
-        <div><h1>삭제<h1></div>
-        <?php
-        $idx = $_REQUEST['idx'];
-        ?>
-        <form name = "del" action = "del_proc.php" method = "post">
-            <input type = "hidden" name = "idx" value = "<?=$idx?>">
-            <div><h3>정말로 삭제하시겠습니까?</h3></div>
-            <div>
-                <input type = "submit" class = "b2" value = "삭제">
-                <a href = "javascript:history.go(-1);"><input type = "button" class = "b2" value = "취소"></a>
-            </div>    
-        </center>
-        </form>      
-    </body>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>게시글 삭제 확인</title>
+    <link rel="stylesheet" href="css/table.css">
+</head>
+<body>
+    <div class="container" style="text-align: center; margin-top: 50px;">
+        <header>
+            <h1>삭제 확인</h1>
+        </header>
+
+        <form name="del" action="del_proc.php" method="post">
+            <input type="hidden" name="idx" value="<?= $idx ?>">
+            
+            <div class="message-box">
+                <h3>정말로 이 게시글을 삭제하시겠습니까?</h3>
+                <p style="color: red; font-size: 0.9em;">삭제된 데이터는 복구할 수 없습니다.</p>
+            </div>
+
+            <div class="btn-group">
+                <button type="submit" class="b2">확인 및 삭제</button>
+                <button type="button" class="b2" onclick="history.back();">취소</button>
+            </div>
+        </form>
+    </div>
+</body>
 </html>
